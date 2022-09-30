8c378712ef10e5e00b8816d69472af9307a9dbc4
Sander Smedbol <sander.smedbol@stud.noroff.no>
2022-09-30 12:04:38 +0200

    asda

---
+++ Javascript-2-CA/vite.config.js
@@ -0,0 +1,12 @@
+const { resolve } = require("path");
+
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, "index.html"),
+        signIn: resolve(__dirname, "sign-in.html"),
+      },
+    },
+  },
+};
